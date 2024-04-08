d05e94476b97a2778759172926239bc6487f5ada
Justin Garner <charlottewebsolutions1@gmail.com>
2024-04-08 00:44:46 -0400

    Re-add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,9 @@
     import "../fonts.css";
     import { onMount } from "svelte";
 
-    /*
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
     inject({ mode: dev ? 'development' : 'production' });
-    */
 
     import posthog from 'posthog-js'
 
