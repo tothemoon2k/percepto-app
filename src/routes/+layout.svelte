7187b2a66265488f88703f9548838da64c75fdfb
Justin Garner <charlottewebsolutions1@gmail.com>
2024-04-08 00:38:23 -0400

    Add posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,18 @@
 <script>
     import "../app.css";
     import "../fonts.css";
+    
+    /*
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
-
     inject({ mode: dev ? 'development' : 'production' });
+    */
+
+    import posthog from 'posthog-js'
+
+    posthog.init('phc_zqngHf3l7HSeQHegUJ4MCDA3Czghpdt222nSuXULBFz', { api_host: 'https://app.posthog.com' })
+
+    //Post hog events - posthog.capture('my event', { property: 'value' })
 </script>
   
 <slot />
