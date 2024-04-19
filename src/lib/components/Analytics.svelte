653fdf4356e6239324736c435f06a8246ad52016
Justin Garner <charlottewebsolutions1@gmail.com>
2024-04-19 03:16:26 -0400

    Fix mouseflow

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -11,15 +11,3 @@
         posthog.init('phc_zqngHf3l7HSeQHegUJ4MCDA3Czghpdt222nSuXULBFz', { api_host: 'https://app.posthog.com' })
     })
 </script>
-
-<svelte:head>
-    <script type="text/javascript">
-        window._mfq = window._mfq || [];
-        (function() {
-          var mf = document.createElement("script");
-          mf.type = "text/javascript"; mf.defer = true;
-          mf.src = "//cdn.mouseflow.com/projects/6249c990-5235-4716-aac9-efe0a2fc358e.js";
-          document.getElementsByTagName("head")[0].appendChild(mf);
-        })();
-    </script>
-</svelte:head>
